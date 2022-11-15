95a824b8964af718fce108c7d6111bc980025c41
Rigoberto Meneses <rmeneses@provincianet.com.ar>
2022-11-15 15:20:49 -0300

    .config2

--- models/config.php
+++ models/config.php
@@ -4,7 +4,7 @@ setlocale (LC_CTYPE, 'es_ES');
 ini_set ('display_errors','1');
 ini_set ('memory_limit','-1');
 
-define("servidor", "localhost"); // Servidor Base de Datos 172.31.28.121
+define("servidor", "localhost"); // Servidor Base de Datos 172.31.28.121     .
 define("basededatos", "prome"); // Nombre de Base de Datos
 define("usuario", "prome"); // Usuario de Base de Datos
 define("clave", "prome"); // Clave de Usuario de Base de Datos
