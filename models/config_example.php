74a34c24ddba22fd256ca06faa03ba3c2bd1cfb2
Lucas Rios Ventosa <lrios@provincianet.com.ar>
2022-12-07 13:40:11 -0300

    added docker file

--- models/config_example.php
+++ models/config_example.php
@@ -4,8 +4,8 @@ setlocale (LC_CTYPE, 'es_ES');
 ini_set ('display_errors','0');
 ini_set ('memory_limit','-1');
 
-define("servidor", "localhost"); // Servidor Base de Datos 172.31.28.121     .
+define("servidor", $_ENV['DB']); // Servidor Base de Datos 172.31.28.121     .
 define("basededatos", "prome"); // Nombre de Base de Datos
-define("usuario", ""); // Usuario de Base de Datos
-define("clave", ""); // Clave de Usuario de Base de Datos
+define("usuario", "prome"); // Usuario de Base de Datos
+define("clave", "prome"); // Clave de Usuario de Base de Datos
 ?>
